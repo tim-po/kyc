3331b30455d6918d1d03f61b26c00817aafe7a6c
Timophey Podolenchuck <timopheypodolenchuck@Timopheys-MacBook-Pro.local>
2022-07-13 02:47:26 +0300

    new connector

--- components/WalletConnector/index.tsx
+++ components/WalletConnector/index.tsx
@@ -7,7 +7,7 @@ import MetamaskJazzicon from "../MetamaskJazzicon";
 import {HidingText} from "../HidingText";
 import {useWeb3React} from "@web3-react/core";
 import useOnClickOutside from "../../hooks/useOnClickOutside";
-import './index.css'
+import './index.scss'
 import {injected, switchNetwork, walletconnect} from "../../wallet";
 
 // CONSTANTS
@@ -33,26 +33,27 @@ const WalletConnector = (props: WalletConnectorPropType) => {
     const ref = useRef(null);
 
     const [isConnectorOpen, setIsConnectorOpen] = useState(false)
-    const [isDisconnectShowing, setIsDisconnectShowing] = useState(false)
+    const [isCopyShowing, setIsCopyShowing] = useState(false)
 
     useOnClickOutside(ref, () => setIsConnectorOpen(false))
 
     function mainButtonClick(){
-        if(!active){
-            setIsConnectorOpen(!isConnectorOpen)
+        setIsConnectorOpen(!isConnectorOpen)
+    }
+
+    function disconect(){
+        setIsConnectorOpen(!isConnectorOpen)
+        // @ts-ignore
+        if(connector && connector.walletConnectProvider){
+            deactivate();
         }else{
-            // @ts-ignore
-            if(connector && connector.walletConnectProvider){
-                deactivate();
-            }else{
-                displayNotification()
-            }
+            displayNotification()
         }
     }
 
     function truncate(str: string) {
         return str.length > 0
-            ? str.substr(2, 3) + "..." + str.substr(str.length - 3, str.length - 1)
+            ? str.substr(0, 8) + "..." + str.substr(str.length - 8, str.length - 1)
             : str;
     }
 
@@ -66,12 +67,22 @@ const WalletConnector = (props: WalletConnectorPropType) => {
         initNetwork();
     }, [active, chainId, error]);
 
+    async function copyTextToClipboard(text: string) {
+        setIsCopyShowing(true)
+        setTimeout(()=>{
+            setIsCopyShowing(false)
+        }, 1500)
+        if ('clipboard' in navigator) {
+            return await navigator.clipboard.writeText(text);
+        } else {
+            return document.execCommand('copy', true, text);
+        }
+    }
+
     return (
         <div className={'disconnect-button-container'} ref={ref}>
             <div
-                onMouseEnter={() => setIsDisconnectShowing(true)}
-                onMouseLeave={() => setIsDisconnectShowing(false)}
-                style={{zIndex: 2, width: '100%'}}
+                style={{zIndex: 2, width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'flex-end'}}
             >
                 {/* @ts-ignore */}
                 <Button
@@ -81,50 +92,110 @@ const WalletConnector = (props: WalletConnectorPropType) => {
                     {active &&
                         <>
                             <MetamaskJazzicon/>
-                            <HidingText className={'account-address'} defaultText={truncate(`${account}`)} hidingText={'Disconnect'}
-                                        peekOut={isDisconnectShowing}/>
+                            Profile
                         </>
                     }
                     {!active &&
-                        <div className={'connect-wallet-flex'}>
-                            <span className={`connect-title ${isConnectorOpen ? 'open': ''}`}>Connect Wallet</span>
-                            <div className={`connector-options ${isConnectorOpen ? 'open': ''}`}>
-                                <div
-                                    className="connection-button"
-                                    onClick={() => {
-                                        activate(injected);
-                                    }}
-                                >
-                                    <img
-                                        src="/images/wallet/metamask.svg"
-                                        alt="metamask"
-                                        width="35"
-                                        height="35"
-                                        style={{marginRight: 10}}
-                                    />
-                                    <p>MetaMask</p>
-                                </div>
-                                <div
-                                    className="connection-button"
-                                    onClick={() => {
-                                        activate(walletconnect).then(()=>{
-                                            window.location.reload()
-                                        });
-                                    }}
-                                >
-                                    <img
-                                        src="/images/wallet/trustwallet.svg"
-                                        alt="metamask"
-                                        width="35"
-                                        height="35"
-                                        style={{marginRight: 10}}
-                                    />
-                                    <p>Wallet connect</p>
-                                </div>
-                            </div>
-                        </div>
+                      <span className={`connect-title ${isConnectorOpen ? 'open': ''}`}>Connect Wallet</span>
                     }
                 </Button>
+                {active &&
+                  <div className={`connect-wallet-flex ${isConnectorOpen ? 'open': ''} ${active ? 'connected': 'not-connected'} `}>
+                      <div className={`connector-options ${isConnectorOpen ? 'open': ''}`}>
+                          <button
+                            className="connection-button"
+                            onClick={() => {
+                                copyTextToClipboard(`${account}`)
+                            }}
+                          >
+                              <div style={{marginRight: 16}}/>
+                              <div style={{marginRight: 12}}/>
+                              <HidingText defaultText={truncate(`${account}`)} hidingText={'Copied!'} peekOut={isCopyShowing}/>
+                          </button>
+                          <button
+                            className="connection-button"
+                            style={{paddingLeft: 0, paddingRight: 0}}
+                          >
+                              <div className={'bordered'}>
+                                  <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                      <circle cx="8" cy="8" r="8" fill="#FFB627"/>
+                                      <path d="M9 5.54595L8.53479 9.75561C8.50498 10.0253 8.27708 10.2294 8.00574 10.2294V10.2294C7.73496 10.2294 7.50733 10.0261 7.47686 9.75703L7 5.54595V4C7 3.44772 7.44772 3 8 3V3C8.55228 3 9 3.44772 9 4V5.54595ZM7.99681 11.0327C8.52262 11.0327 8.94888 11.4589 8.94888 11.9848V12.0479C8.94888 12.5737 8.52262 13 7.99681 13V13C7.47099 13 7.04473 12.5737 7.04473 12.0479V11.9848C7.04473 11.4589 7.47099 11.0327 7.99681 11.0327V11.0327Z" fill="white"/>
+                                  </svg>
+                                  <div style={{marginRight: 12}}/>
+                                  Verify personal data
+                                  <div style={{marginRight: 12}}/>
+                                  <svg width="19" height="16" viewBox="0 0 19 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                      <path d="M18.7071 8.70711C19.0976 8.31658 19.0976 7.68342 18.7071 7.29289L12.3431 0.928932C11.9526 0.538408 11.3195 0.538408 10.9289 0.928932C10.5384 1.31946 10.5384 1.95262 10.9289 2.34315L16.5858 8L10.9289 13.6569C10.5384 14.0474 10.5384 14.6805 10.9289 15.0711C11.3195 15.4616 11.9526 15.4616 12.3431 15.0711L18.7071 8.70711ZM0 9L18 9V7L0 7L0 9Z" fill="white"/>
+                                  </svg>
+
+                              </div>
+                          </button>
+                          <button
+                            className="connection-button"
+                            onClick={() => {
+                                alert(2)
+                            }}
+                          >
+                              <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                  <path d="M0 14C0 11.2386 2.23858 9 5 9H11C13.7614 9 16 11.2386 16 14V14C16 15.1046 15.1046 16 14 16H2C0.895431 16 0 15.1046 0 14V14Z" fill="white"/>
+                                  <rect x="4" width="8" height="8" rx="4" fill="white"/>
+                              </svg>
+                              <div style={{marginRight: 12}}/>
+                              Collection
+                          </button>
+                          <button
+                            className="connection-button"
+                            // style={{color: 'red', fontWeight: 'bold'}}
+                            onClick={disconect}
+                          >
+                              <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                  <rect x="1" y="1" width="14" height="14" rx="7" stroke="white" stroke-width="2"/>
+                                  <rect x="2" y="3.41431" width="2" height="15" transform="rotate(-45 2 3.41431)" fill="white"/>
+                              </svg>
+                              <div style={{marginRight: 12}}/>
+                              Disconnect wallet
+                          </button>
+                      </div>
+                  </div>
+                }
+                {!active &&
+                  <div className={'connect-wallet-flex'}>
+                      <div className={`connector-options ${isConnectorOpen ? 'open': ''}`}>
+                          <div
+                            className="connection-button"
+                            onClick={() => {
+                                activate(injected);
+                            }}
+                          >
+                              <img
+                                src="/images/wallet/metamask.svg"
+                                alt="metamask"
+                                width="35"
+                                height="35"
+                                style={{marginRight: 10}}
+                              />
+                              <p>MetaMask</p>
+                          </div>
+                          <div
+                            className="connection-button"
+                            onClick={() => {
+                                activate(walletconnect).then(()=>{
+                                    window.location.reload()
+                                });
+                            }}
+                          >
+                              <img
+                                src="/images/wallet/trustwallet.svg"
+                                alt="metamask"
+                                width="35"
+                                height="35"
+                                style={{marginRight: 10}}
+                              />
+                              <p>Wallet connect</p>
+                          </div>
+                      </div>
+                  </div>
+                }
             </div>
         </div>
     )
